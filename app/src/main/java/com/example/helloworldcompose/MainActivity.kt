eba9964571d078da7c64376be2d9798b972c4104
luis.varela <luis.varela@altia.es>
2023-09-28 17:36:24 +0200

    class 1/15

--- app/src/main/java/com/example/helloworldcompose/MainActivity.kt
+++ app/src/main/java/com/example/helloworldcompose/MainActivity.kt
@@ -3,30 +3,39 @@ package com.example.helloworldcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloworldcompose.ui.theme.HelloWorldComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorldComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            HelloApp()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HelloApp(){
+    HelloWorldComposeTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExampleModifier(){
+    Text(text = "Subscríbete", modifier = Modifier.padding(horizontal = 16.dp))
 }
 
 @Composable
