426ae7d8987c94c7620432a9e40ed82dc934bd14
luis.varela <luis.varela@altia.es>
2023-09-29 12:00:06 +0200

    class 4/15

--- app/src/main/java/com/example/helloworldcompose/MainActivity.kt
+++ app/src/main/java/com/example/helloworldcompose/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -30,13 +32,28 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun App() {
-    LazyColumn(modifier = Modifier.fillMaxSize().background(Color.Red)) {
+    var counter by rememberSaveable { mutableStateOf(0) }
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+            .padding(16.dp)
+    ) {
         item {
             Image(
-                modifier = Modifier.fillMaxWidth().height(400.dp),
+                modifier = Modifier.fillMaxWidth(),
                 painter = painterResource(id = R.drawable.ic_launcher_foreground),
                 contentDescription = "Logo Android"
             )
+            Row (modifier = Modifier.padding(top = 8.dp)){
+                Image(
+                    painter = painterResource(id = R.drawable.ic_favorite),
+                    contentDescription = "like",
+                    modifier = Modifier.clickable { counter++ }
+                )
+
+                Text(text = counter.toString(), color = Color.White, modifier = Modifier.padding(start = 4.dp))
+            }
             Text(
                 text = "AristiDevs",
                 fontSize = 32.sp,
@@ -44,25 +61,32 @@ fun App() {
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
-            Text(text = "Subscribe", color = Color.White)
-            Text(text = "Hello", color = Color.White)
+            Text(
+                text = "Android Dev",
+                color = Color.White,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Text(
+                text = "6 years exp",
+                color = Color.White,
+                modifier = Modifier.padding(top = 16.dp)
+            )
             LazyRow(
                 horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth().padding(16.dp)
+                modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
             ) {
                 item {
-                    Text(text = "Stack:", color = Color.White)
-                    Text(text = "Java", color = Color.White)
-                    Text(text = "KOTLIN", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
-                    Text(text = "SUBSCRIBE", color = Color.White)
+                    Text(
+                        text = "Stack:",
+                        color = Color.White,
+                        modifier = Modifier.padding(end = 5.dp)
+                    )
+                    Text(
+                        text = "Java",
+                        color = Color.White,
+                        modifier = Modifier.padding(end = 5.dp)
+                    )
+                    Text(text = "Kotlin", color = Color.White)
                 }
             }
         }
