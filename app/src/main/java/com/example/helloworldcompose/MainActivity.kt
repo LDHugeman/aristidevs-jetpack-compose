87a97b7fe9e8c023ff2109688fc282e99a02a286
luis.varela <luis.varela@altia.es>
2023-09-29 11:02:43 +0200

    class 2/15

--- app/src/main/java/com/example/helloworldcompose/MainActivity.kt
+++ app/src/main/java/com/example/helloworldcompose/MainActivity.kt
@@ -3,50 +3,49 @@ package com.example.helloworldcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.helloworldcompose.ui.theme.HelloWorldComposeTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloApp()
+            App()
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun HelloApp(){
-    HelloWorldComposeTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Greeting("Android")
+fun App() {
+    LazyColumn(modifier = Modifier.fillMaxSize().background(Color.Red)) {
+        item {
+            Image(
+                modifier = Modifier.fillMaxWidth().height(400.dp),
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Logo Android"
+            )
+            Text(
+                text = "AristiDevs",
+                fontSize = 32.sp,
+                color = Color.White,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            Text(text = "Subscribe", color = Color.White)
+            Text(text = "Hello", color = Color.White)
         }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun ExampleModifier(){
-    Text(text = "Subscríbete", modifier = Modifier.padding(horizontal = 16.dp))
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HelloWorldComposeTheme {
-        Greeting("Android")
-    }
 }
